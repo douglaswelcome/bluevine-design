a7c4aecab0a416d11e1ca90c1c298c445c73f271
douglaswelcome <douglaswelcome@gmail.com>
2023-11-09 11:47:42 -0800

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,10 @@ import tailwind from "@astrojs/tailwind";
 
 const SERVER_PORT = 3333;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = 'https://douglaswelcome.github.io';
+let LIVE_URL = 'https://douglaswelcome.github.io';
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-const BASE_URL = LOCALHOST_URL;
+let BASE_URL = LOCALHOST_URL;
 
 if (isBuild) {
   BASE_URL = LIVE_URL;
